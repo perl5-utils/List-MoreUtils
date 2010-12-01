0fcbe76c3a902849f7f57a02602fed32bd20d78b
Adam Kennedy <adamk@cpan.org>
2010-12-01 10:48:05 +0000

    Updating $VERSION
    git-svn-id: http://svn.ali.as/cpan/trunk/List-MoreUtils@13297 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/List/MoreUtils.pm
+++ lib/List/MoreUtils.pm
@@ -7,7 +7,7 @@ use DynaLoader ();
 
 use vars qw{$VERSION @ISA @EXPORT_OK %EXPORT_TAGS};
 BEGIN {
-    $VERSION     = '0.27_01';
+    $VERSION     = '0.27_02';
     @ISA         = qw{ Exporter DynaLoader };
     %EXPORT_TAGS = (
         all => [ qw{
