7b826a3b27dbdb3a58ba4f9eb48d991f1793cec9
Jens Rehsack <sno@netbsd.org>
2014-02-25 20:30:31 +0100

    reworking XS enabling check

--- inc/Config/AutoConf/LMU.pm
+++ inc/Config/AutoConf/LMU.pm
@@ -23,16 +23,43 @@ sub _default_headers_with_perl
     return $includes;
 }
 
+sub _set_argv
+{
+    my ( $self, @argv ) = @_;
+    $self = $self->_get_instance;
+    $self->{_argv} = \@argv;
+    return;
+}
+
 sub check_sane_xs
 {
-    my ( $self ) = @_;
+    my ($self) = @_;
 
-    my $includes    = $self->_default_headers_with_perl();
-    my $conftest    = $self->lang_build_program( $includes, "" );
+    my $includes = $self->_default_headers_with_perl();
+    my $conftest = $self->lang_build_program( $includes, "" );
 
     my $sanexs = $self->compile_if_else($conftest);
 
     return $sanexs;
+}
+
+sub with_xs
+{
+    my $self       = shift->_get_instance;
+    my $cache_name = $self->_cache_name(qw(config with xs));
+    return $self->check_cached(
+        $cache_name,
+        "whether enable xs or not",
+        sub {
+            foreach ( @{ $self->{_argv} } )
+            {
+                /^-pm/ and return 0;
+                /^-xs/ and return 1;
+            }
+
+            return $self->check_sane_xs();
+        }
+    );
 }
 
 1;
