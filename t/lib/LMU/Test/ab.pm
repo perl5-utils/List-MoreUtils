1b6ad03b2155eaf7b9dd93050098db6e516e837d
Jens Rehsack <sno@netbsd.org>
2014-03-18 09:39:36 +0100

    add test for RT#76749 - pairwise issues warnings
    RT#76749 - pairwise issues warnings about $a and $b being used only once
    Test added to prove

---
+++ t/lib/LMU/Test/ab.pm
@@ -0,0 +1,24 @@
+package LMU::Test::ab;
+
+use strict;
+BEGIN {
+	$|  = 1;
+}
+
+use Test::More;
+use List::MoreUtils 'pairwise';
+
+sub run_tests {
+    test_ab();
+    done_testing();
+}
+
+sub test_ab
+{
+    my @A = (1, 2, 3, 4, 5);
+    my @B = (2, 4, 6, 8, 10);
+    my @C = pairwise { $a + $b } @A, @B;
+    is_deeply( \@C, [ 3, 6, 9, 12, 15 ], "pw1" );
+}
+
+1;
