3d944a4d207d395217faeb1734b5c9566ceb6e4b
Jens Rehsack <sno@netbsd.org>
2016-04-13 11:29:08 +0200

    lib: Update copyright year
    Since some improvements were done this year, update copyright accordingly ...
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils/PP.pm
+++ lib/List/MoreUtils/PP.pm
@@ -576,7 +576,7 @@ Some parts copyright 2011 Aaron Crane.
 
 Copyright 2004 - 2010 by Tassilo von Parseval
 
-Copyright 2013 - 2015 by Jens Rehsack
+Copyright 2013 - 2016 by Jens Rehsack
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.4 or,
