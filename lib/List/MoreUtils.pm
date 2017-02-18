84e55205e1711c3f69df8e922c657338316d1170
Jens Rehsack <sno@netbsd.org>
2017-02-18 21:02:58 +0100

    lib/List/MoreUtils.pm: improve loading XS module
    Import functions either from separately installed XS module or from bundled PP
    version.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils.pm
+++ lib/List/MoreUtils.pm
@@ -4,10 +4,17 @@ use 5.008_001;
 use strict;
 use warnings;
 
+my $have_xs;
+
 BEGIN
 {
     our $VERSION = '0.417_001';
-    eval { require List::MoreUtils::XS; } unless $ENV{LIST_MOREUTILS_PP};
+    unless (defined($have_xs))
+    {
+        eval { require List::MoreUtils::XS; } unless $ENV{LIST_MOREUTILS_PP};
+        die $@ if $@ && defined $ENV{LIST_MOREUTILS_PP} && $ENV{LIST_MOREUTILS_PP} == 0;
+        $have_xs = 0+defined( $INC{'List/MoreUtils/XS.pm'});
+    }
 
     use List::MoreUtils::PP qw();
 }
@@ -35,10 +42,18 @@ my @v0_400 = qw(one any_u all_u none_u notall_u one_u
   singleton bsearchidx
 );
 
-my @all_functions = ( @junctions, @v0_22, @v0_24, @v0_33, @v0_400 );
+my @all_functions = (@junctions, @v0_22, @v0_24, @v0_33, @v0_400);
 
 no strict "refs";
-List::MoreUtils->can($_) or *$_ = List::MoreUtils::PP->can($_) for(@all_functions);
+if ($have_xs)
+{
+    my $x;
+    for (@all_functions)
+    {
+        List::MoreUtils->can($_) or *$_ = $x if ($x = List::MoreUtils::XS->can($_));
+    }
+}
+List::MoreUtils->can($_) or *$_ = List::MoreUtils::PP->can($_) for (@all_functions);
 use strict;
 
 my %alias_list = (
@@ -63,42 +78,42 @@ my %alias_list = (
 );
 
 our @ISA         = qw(Exporter::Tiny);
-our @EXPORT_OK   = ( @all_functions, map { keys %$_ } values %alias_list );
+our @EXPORT_OK   = (@all_functions, map { keys %$_ } values %alias_list);
 our %EXPORT_TAGS = (
     all         => \@EXPORT_OK,
     'like_0.22' => [
-        any_u    => { -as => 'any' },
-        all_u    => { -as => 'all' },
-        none_u   => { -as => 'none' },
-        notall_u => { -as => 'notall' },
+        any_u    => {-as => 'any'},
+        all_u    => {-as => 'all'},
+        none_u   => {-as => 'none'},
+        notall_u => {-as => 'notall'},
         @v0_22,
-        keys %{ $alias_list{v0_22} },
+        keys %{$alias_list{v0_22}},
     ],
     'like_0.24' => [
-        any_u    => { -as => 'any' },
-        all_u    => { -as => 'all' },
-        notall_u => { -as => 'notall' },
+        any_u    => {-as => 'any'},
+        all_u    => {-as => 'all'},
+        notall_u => {-as => 'notall'},
         'none',
         @v0_22,
         @v0_24,
-        keys %{ $alias_list{v0_22} },
+        keys %{$alias_list{v0_22}},
     ],
     'like_0.33' => [
         @junctions,
         @v0_22,
         # v0_24 functions were omitted
         @v0_33,
-        keys %{ $alias_list{v0_22} },
-        keys %{ $alias_list{v0_33} },
+        keys %{$alias_list{v0_22}},
+        keys %{$alias_list{v0_33}},
     ],
 );
 
-for my $set ( values %alias_list )
+for my $set (values %alias_list)
 {
-    for my $alias ( keys %$set )
+    for my $alias (keys %$set)
     {
         no strict qw(refs);
-        *$alias = __PACKAGE__->can( $set->{$alias} );
+        *$alias = __PACKAGE__->can($set->{$alias});
     }
 }
 
