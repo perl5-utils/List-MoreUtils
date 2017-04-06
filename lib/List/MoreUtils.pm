d610c14a4d024c9df16802b8c762b324da3b89ac
Jens Rehsack <sno@netbsd.org>
2017-04-06 14:08:36 +0200

    Changes, *.pm: prepare for release
    Bump version and note the made change to release a new version fixing
    the windows tooling.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils.pm
+++ lib/List/MoreUtils.pm
@@ -8,7 +8,7 @@ my $have_xs;
 
 BEGIN
 {
-    our $VERSION = '0.418';
+    our $VERSION = '0.419';
     unless (defined($have_xs))
     {
         eval { require List::MoreUtils::XS; } unless $ENV{LIST_MOREUTILS_PP};
--- lib/List/MoreUtils/PP.pm
+++ lib/List/MoreUtils/PP.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.418';
+our $VERSION = '0.419';
 
 =pod
 
