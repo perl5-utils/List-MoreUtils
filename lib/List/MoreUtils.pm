31a7c50f9fafd1cfb24e622d79368d97edc39400
Jens Rehsack <sno@netbsd.org>
2017-08-22 10:09:54 +0200

    Changes: prepare for release
    Bump version and note important changes.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils.pm
+++ lib/List/MoreUtils.pm
@@ -8,7 +8,7 @@ my $have_xs;
 
 BEGIN
 {
-    our $VERSION = '0.420_001';
+    our $VERSION = '0.423';
     unless (defined($have_xs))
     {
         eval { require List::MoreUtils::XS; } unless $ENV{LIST_MOREUTILS_PP};
