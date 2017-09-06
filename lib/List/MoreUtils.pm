83cf53d1f5c9d07dc3927e1220a88e814e0c3e12
Jens Rehsack <sno@netbsd.org>
2017-09-06 10:36:08 +0200

    Changes: prepare for release
    Note changes made and bump version for a recommend -> prereq_pm change
    requested quickly by Johan Lindstrom and Andreas Koenig in RT#122875.
    Both strongly voted for a revert to 0.429 which would hurt all users
    of new functions, so the only reasonable offer is dealing better with
    some tool issues.
    
    It was an error to split PP and XS to get rid of Config::AutoConf in
    base distribution - but the error has been made and is in the world,
    so a rollback is not possible. Discussing this is insane - looking
    forward is the only option.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils.pm
+++ lib/List/MoreUtils.pm
@@ -8,7 +8,7 @@ my $have_xs;
 
 BEGIN
 {
-    our $VERSION = '0.424_001';
+    our $VERSION = '0.425';
     unless (defined($have_xs))
     {
         eval { require List::MoreUtils::XS; } unless $ENV{LIST_MOREUTILS_PP};
--- lib/List/MoreUtils/PP.pm
+++ lib/List/MoreUtils/PP.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.424_001';
+our $VERSION = '0.425';
 
 =pod
 
